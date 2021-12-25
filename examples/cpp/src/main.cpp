12f4662d9a1ad19b3df993cc6e2697b5a6e5760b
NIRAJ DESAI <niraj@app.st>
2021-12-25 10:51:00 -0800

    Add cpp examples for atlas-genpack-cpp

--- examples/cpp/src/main.cpp
+++ examples/cpp/src/main.cpp
@@ -1,6 +1,14 @@
-#include <iostream>
+// #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
-    return 0;
+// int main(int argc, const char * argv[]) {
+//     std::cout << "Hello, World!\n";
+//     return 0;
+// }
+
+#include <grpc/grpc.h>
+
+int main() {
+    grpc_init();
+    grpc_shutdown();
+    return GRPC_STATUS_OK;
 }
